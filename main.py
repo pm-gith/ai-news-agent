69cc510995876d8b347e4f23bcdd383a2b6fc4b0
pm-gith <117115925+pm-gith@users.noreply.github.com>
2025-07-21 16:59:34 -0700

    Resolved merge conflicts from feature/clickable-links

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ import schedule
 import time
 
 #automate
-<<<<<<< HEAD
-schedule.every().day.at("15:53").do(run_agent)
-=======
 schedule.every().day.at("16:51").do(run_agent)
->>>>>>> feature/clickable-links
 
 while True:
     schedule.run_pending()
